2152e4d4c0e7fbba7d008bd989d9511101e93caf
Leo <leonardo@2up.com.br>
2022-10-17 18:35:23 -0300

    v0.2.0 - Correções Layout + Video Chamada

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -7,6 +7,6 @@ const options = {
     transports: ["websocket"]
 }
 
-const socket = io('http://localhost:3001', options);
+const socket = io('https://2up-telemedicina-2022.vercel.app:3001', options);
 
 export default socket;
